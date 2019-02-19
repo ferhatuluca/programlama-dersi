3d082140f774989d963e0e54ae3cd72cb6d6992b
Ferhat Uluca <34395388+ferhatuluca@users.noreply.github.com>
2019-02-19 20:39:29 +0300

    Create recursive_non-recursive.py

---
+++ recursive_non-recursive.py
@@ -0,0 +1,54 @@
+def fibonacci(n):
+  a,b=0,1
+  if(n==0):
+    return 0
+  for i in range(n-1):
+    c=a+b
+    a=b        # veya a,b=b,a+b
+    b=c
+  return b
+
+def fibonacci_recursive(n): 
+  if(n<2):
+    return n
+  else:
+    return fibonacci_recursive(n-1) + fibonacci_recursive(n-2)
+
+  #--------------------------------------------------
+  
+
+def factoriyel(n):
+  s=1
+  for i in range(1,n+1):
+    s=s*i
+  return s
+
+def factoriyel_recursive(n):
+  if(n==1):
+    return 1
+  else:
+    return n*factoriyel_recursive(n-1)
+    
+  #--------------------------------------------------
+  #facktoriyelde recursive olsa bile yüksek rakamlarda hesaplıyor
+
+def power(m,n):
+  s=1
+  for i in range(n):
+    s=s*m
+  return s
+
+def power_recursive(m,n):
+  if(n==0):
+    return 1
+  elif(n==1):
+    return m
+  elif(n%2==0):
+    return power_recursive(m*m,int(n/2))
+  elif(n%2!=0):
+    return power_recursive(m*m,int(n/2))*m
+
+
+print(factoriyel(4))
+print(factoriyel_recursive(4))
+print(power_recursive(25,2))
