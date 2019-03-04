cc32a2d1d337c26bf0cfc52756867c89769c8872
Ferhat Uluca <34395388+ferhatuluca@users.noreply.github.com>
2019-03-04 18:41:16 +0300

    Create Vektörlerde_işlemler.py

---
+++ Vektörlerde_işlemler.py
@@ -0,0 +1,56 @@
+""" İki vektörün vektörel çarpımı. """
+
+def Vectors_Inner_Product(v,m):
+  size=len(v)
+  my_result=[]
+  for i in range(size):
+    my_result.append(0)
+  for i in range(size):
+    my_result[i]=v[i]*w[i]
+  
+  t=0
+  for i in range(size):
+    t=t+my_result[i]
+  return t
+
+""" İki vektörün skaler çarpımı. """
+
+def Vectors_scalar_product(alpha,v):
+  size=len(v)
+  my_result=[]
+  for i in range(size):
+    my_result.append(0)
+  for i in range(size):
+    my_result[i]=alpha*v[i]
+  return my_result
+
+""" İki vektörün birbirinden çıkarılması. """
+
+def Vektors_substraction(v,w):
+  size=len(v)
+  my_result=[]
+  for i in range(size):
+    my_result.append(0)
+  for i in range(size):
+    my_result[i]=v[i]-w[i]
+  return my_result
+  
+""" İki vektörün birbirine eklenmesi. """
+
+def Vectors_Addition(v,w):
+  size=len(v)
+  my_result=[]
+  for i in range(size):
+    my_result.append(0)
+  for i in range(size):
+    my_result[i]=v[i]+w[i]
+  return my_result
+
+#---------------------------------------
+v=[1,2,3,4,5]
+w=[8,3,5,7,6]
+alpha=8
+print(Vectors_Inner_Product(v,w))
+print(Vectors_scalar_product(alpha,v))
+print(Vektors_substraction(v,w))
+print(Vectors_Addition(v,w))
